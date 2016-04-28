5014000cece047b30f319e98f6cbc6c4bc49a43d
MaidSafe-QA <qa@maidsafe.net>
2016-04-28 11:20:39 +0000

    Updated documentation.

--- latest/implementors/std/sys_common/thread_info/trait.NewThread.js
+++ latest/implementors/std/sys_common/thread_info/trait.NewThread.js
@@ -1,5 +1,5 @@
 (function() {var implementors = {};
-implementors['socket_addr'] = [];implementors['mio'] = [];implementors['crust'] = [];
+implementors['bitflags'] = [];implementors['socket_addr'] = [];implementors['mio'] = [];implementors['crust'] = [];
 
             if (window.register_implementors) {
                 window.register_implementors(implementors);
