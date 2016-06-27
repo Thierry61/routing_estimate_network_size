1debd7c45e4d82f4aa9316679c96a51d921e2caf
MaidSafe-QA <qa@maidsafe.net>
2016-06-27 17:16:19 +0000

    Updated documentation.

--- master/implementors/collections/borrow/trait.ToOwned.js
+++ master/implementors/collections/borrow/trait.ToOwned.js
@@ -1,5 +1,5 @@
 (function() {var implementors = {};
-implementors['regex_syntax'] = [];implementors['sodiumoxide_extras'] = [];implementors['mio'] = [];implementors['tmp_mio'] = [];implementors['serde'] = [];implementors['hyper'] = [];implementors['maidsafe_utilities'] = [];implementors['igd'] = [];implementors['crust'] = [];
+implementors['regex_syntax'] = [];implementors['sodiumoxide_extras'] = [];implementors['serde'] = [];implementors['mio'] = [];implementors['tmp_mio'] = [];implementors['hyper'] = [];implementors['igd'] = [];implementors['maidsafe_utilities'] = [];implementors['crust'] = [];
 
             if (window.register_implementors) {
                 window.register_implementors(implementors);
