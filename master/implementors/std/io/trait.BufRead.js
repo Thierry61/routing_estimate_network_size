00a6e45d13e9e285bf9df195136afbe3c05a2301
MaidSafe-QA <qa@maidsafe.net>
2016-07-01 10:44:56 +0000

    Updated documentation.

--- master/implementors/std/io/trait.BufRead.js
+++ master/implementors/std/io/trait.BufRead.js
@@ -1,5 +1,5 @@
 (function() {var implementors = {};
-implementors['sodiumoxide_extras'] = [];implementors['tmp_mio'] = [];implementors['mio'] = [];implementors['maidsafe_utilities'] = [];implementors['hyper'] = ["impl&lt;R: <a class='trait' href='https://doc.rust-lang.org/nightly/std/io/trait.Read.html' title='std::io::Read'>Read</a>&gt; <a class='trait' href='https://doc.rust-lang.org/nightly/std/io/trait.BufRead.html' title='std::io::BufRead'>BufRead</a> for BufReader&lt;R&gt;",];implementors['crust'] = [];
+implementors['sodiumoxide_extras'] = [];implementors['mio'] = [];implementors['tmp_mio'] = [];implementors['maidsafe_utilities'] = [];implementors['hyper'] = ["impl&lt;R: <a class='trait' href='https://doc.rust-lang.org/nightly/std/io/trait.Read.html' title='std::io::Read'>Read</a>&gt; <a class='trait' href='https://doc.rust-lang.org/nightly/std/io/trait.BufRead.html' title='std::io::BufRead'>BufRead</a> for BufReader&lt;R&gt;",];implementors['crust'] = [];
 
             if (window.register_implementors) {
                 window.register_implementors(implementors);
