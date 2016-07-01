8133a10b070b650eed67e60ce47b4f1c5929d4a9
MaidSafe-QA <qa@maidsafe.net>
2016-07-01 12:09:15 +0000

    Updated documentation.

--- master/implementors/collections/slice/trait.SliceConcatExt.js
+++ master/implementors/collections/slice/trait.SliceConcatExt.js
@@ -1,5 +1,5 @@
 (function() {var implementors = {};
-implementors['regex_syntax'] = [];implementors['sodiumoxide_extras'] = [];implementors['mio'] = [];implementors['tmp_mio'] = [];implementors['serde'] = [];implementors['maidsafe_utilities'] = [];implementors['hyper'] = [];implementors['igd'] = [];implementors['crust'] = [];
+implementors['regex_syntax'] = [];implementors['sodiumoxide_extras'] = [];implementors['serde'] = [];implementors['mio'] = [];implementors['tmp_mio'] = [];implementors['maidsafe_utilities'] = [];implementors['hyper'] = [];implementors['igd'] = [];implementors['crust'] = [];
 
             if (window.register_implementors) {
                 window.register_implementors(implementors);
