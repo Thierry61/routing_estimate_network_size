a3b6e935268e497fff008de1b7cb1ea13492bc13
MaidSafe-QA <qa@maidsafe.net>
2016-07-27 16:45:30 +0000

    Updated documentation.

--- latest/implementors/core/str/pattern/trait.DoubleEndedSearcher.js
+++ latest/implementors/core/str/pattern/trait.DoubleEndedSearcher.js
@@ -1,5 +1,5 @@
 (function() {var implementors = {};
-implementors["lazy_static"] = [];implementors["regex_syntax"] = [];implementors["libc"] = [];implementors["sodiumoxide_extras"] = [];implementors["mio"] = [];implementors["tmp_mio"] = [];implementors["serde"] = [];implementors["igd"] = [];implementors["maidsafe_utilities"] = [];implementors["crust"] = [];
+implementors["lazy_static"] = [];implementors["regex_syntax"] = [];implementors["libc"] = [];implementors["sodiumoxide_extras"] = [];implementors["mio"] = [];implementors["serde"] = [];implementors["tmp_mio"] = [];implementors["maidsafe_utilities"] = [];implementors["igd"] = [];implementors["crust"] = [];
 
             if (window.register_implementors) {
                 window.register_implementors(implementors);
