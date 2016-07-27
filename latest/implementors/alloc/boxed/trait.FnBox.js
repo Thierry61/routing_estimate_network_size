ae7831d5b9859be287726b8d18fcdf5c5c42adbe
MaidSafe-QA <qa@maidsafe.net>
2016-07-27 15:56:05 +0000

    Updated documentation.

--- latest/implementors/alloc/boxed/trait.FnBox.js
+++ latest/implementors/alloc/boxed/trait.FnBox.js
@@ -1,5 +1,5 @@
 (function() {var implementors = {};
-implementors["regex_syntax"] = [];implementors["sodiumoxide_extras"] = [];implementors["tmp_mio"] = [];implementors["mio"] = [];implementors["serde"] = [];implementors["igd"] = [];implementors["maidsafe_utilities"] = [];implementors["crust"] = [];
+implementors["regex_syntax"] = [];implementors["sodiumoxide_extras"] = [];implementors["mio"] = [];implementors["tmp_mio"] = [];implementors["serde"] = [];implementors["igd"] = [];implementors["maidsafe_utilities"] = [];implementors["crust"] = [];
 
             if (window.register_implementors) {
                 window.register_implementors(implementors);
