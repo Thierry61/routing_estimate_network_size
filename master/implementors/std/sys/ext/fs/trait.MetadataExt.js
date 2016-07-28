3d2d0a0882721daa9edb5d8561493bee9e3b1d3d
MaidSafe-QA <qa@maidsafe.net>
2016-07-28 14:29:07 +0000

    Updated documentation.

--- master/implementors/std/sys/ext/fs/trait.MetadataExt.js
+++ master/implementors/std/sys/ext/fs/trait.MetadataExt.js
@@ -1,5 +1,5 @@
 (function() {var implementors = {};
-implementors["sodiumoxide_extras"] = [];implementors["openssl"] = [];implementors["mio"] = [];implementors["tmp_mio"] = [];implementors["maidsafe_utilities"] = [];implementors["crust"] = [];
+implementors["sodiumoxide_extras"] = [];implementors["openssl"] = [];implementors["tmp_mio"] = [];implementors["mio"] = [];implementors["maidsafe_utilities"] = [];implementors["crust"] = [];
 
             if (window.register_implementors) {
                 window.register_implementors(implementors);
