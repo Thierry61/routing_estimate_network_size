c4bfa8bf37d981c8576df75bcc61721ffbc695e4
MaidSafe-QA <qa@maidsafe.net>
2016-08-03 13:45:53 +0000

    Updated documentation.

--- master/implementors/core/str/pattern/trait.Searcher.js
+++ master/implementors/core/str/pattern/trait.Searcher.js
@@ -1,5 +1,5 @@
 (function() {var implementors = {};
-implementors["lazy_static"] = [];implementors["regex_syntax"] = [];implementors["libc"] = [];implementors["sodiumoxide_extras"] = [];implementors["openssl"] = [];implementors["tmp_mio"] = [];implementors["mio"] = [];implementors["serde"] = [];implementors["maidsafe_utilities"] = [];implementors["igd"] = [];implementors["crust"] = [];
+implementors["lazy_static"] = [];implementors["libc"] = [];implementors["regex_syntax"] = [];implementors["openssl"] = [];implementors["sodiumoxide_extras"] = [];implementors["tmp_mio"] = [];implementors["mio"] = [];implementors["serde"] = [];implementors["maidsafe_utilities"] = [];implementors["igd"] = [];implementors["crust"] = [];
 
             if (window.register_implementors) {
                 window.register_implementors(implementors);
