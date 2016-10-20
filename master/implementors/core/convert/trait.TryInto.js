7d4149839f5cc18ce8e20f275fb3d300893230e6
MaidSafe-QA <qa@maidsafe.net>
2016-10-20 10:49:34 +0000

    Updated documentation.

--- master/implementors/core/convert/trait.TryInto.js
+++ master/implementors/core/convert/trait.TryInto.js
@@ -1,5 +1,5 @@
 (function() {var implementors = {};
-implementors["lazy_static"] = [];implementors["antidote"] = [];implementors["either"] = [];implementors["humantime"] = [];implementors["libc"] = [];implementors["openssl"] = [];implementors["rust_sodium_sys"] = [];implementors["serde"] = [];implementors["tmp_mio"] = [];implementors["mio"] = [];implementors["hyper"] = [];implementors["serde"] = [];implementors["maidsafe_utilities"] = [];implementors["crust"] = [];
+implementors["lazy_static"] = [];implementors["antidote"] = [];implementors["either"] = [];implementors["humantime"] = [];implementors["libc"] = [];implementors["rust_sodium_sys"] = [];implementors["openssl"] = [];implementors["mio"] = [];implementors["tmp_mio"] = [];implementors["serde"] = [];implementors["hyper"] = [];implementors["serde"] = [];implementors["maidsafe_utilities"] = [];implementors["crust"] = [];
 
             if (window.register_implementors) {
                 window.register_implementors(implementors);
