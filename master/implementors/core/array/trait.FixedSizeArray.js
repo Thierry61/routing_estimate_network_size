0d33ca01163ce02939d1bb2325c4e960cc8db0c7
MaidSafe-QA <qa@maidsafe.net>
2016-10-25 16:36:58 +0000

    Updated documentation.

--- master/implementors/core/array/trait.FixedSizeArray.js
+++ master/implementors/core/array/trait.FixedSizeArray.js
@@ -1,5 +1,5 @@
 (function() {var implementors = {};
-implementors["lazy_static"] = [];implementors["either"] = [];implementors["libc"] = [];implementors["serde"] = [];implementors["hyper"] = [];implementors["serde"] = [];
+implementors["either"] = [];implementors["lazy_static"] = [];implementors["libc"] = [];implementors["serde"] = [];implementors["hyper"] = [];implementors["serde"] = [];
 
             if (window.register_implementors) {
                 window.register_implementors(implementors);
