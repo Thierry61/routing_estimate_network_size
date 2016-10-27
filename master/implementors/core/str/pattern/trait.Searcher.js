6fc7066a1443c0cf3a3b362e5696b42d5baa98be
MaidSafe-QA <qa@maidsafe.net>
2016-10-27 11:07:43 +0000

    Updated documentation.

--- master/implementors/core/str/pattern/trait.Searcher.js
+++ master/implementors/core/str/pattern/trait.Searcher.js
@@ -1,5 +1,5 @@
 (function() {var implementors = {};
-implementors["either"] = [];implementors["antidote"] = [];implementors["lazy_static"] = [];implementors["humantime"] = [];implementors["regex_syntax"] = [];implementors["libc"] = [];implementors["rust_sodium_sys"] = [];implementors["openssl"] = [];implementors["tmp_mio"] = [];implementors["mio"] = [];implementors["serde"] = [];implementors["serde"] = [];implementors["maidsafe_utilities"] = [];implementors["igd"] = [];implementors["crust"] = [];
+implementors["antidote"] = [];implementors["humantime"] = [];implementors["lazy_static"] = [];implementors["either"] = [];implementors["regex_syntax"] = [];implementors["libc"] = [];implementors["rust_sodium_sys"] = [];implementors["openssl"] = [];implementors["mio"] = [];implementors["tmp_mio"] = [];implementors["serde"] = [];implementors["serde"] = [];implementors["maidsafe_utilities"] = [];implementors["igd"] = [];implementors["crust"] = [];
 
             if (window.register_implementors) {
                 window.register_implementors(implementors);
