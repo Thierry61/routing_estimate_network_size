b0433acd9fd436aa285d5eed49d47efa819a057f
MaidSafe-QA <qa@maidsafe.net>
2016-10-27 15:28:11 +0000

    Updated documentation.

--- master/implementors/core/ops/trait.Fn.js
+++ master/implementors/core/ops/trait.Fn.js
@@ -1,5 +1,5 @@
 (function() {var implementors = {};
-implementors["antidote"] = [];implementors["humantime"] = [];implementors["lazy_static"] = [];implementors["either"] = [];implementors["libc"] = [];implementors["rust_sodium_sys"] = [];implementors["openssl"] = [];implementors["mio"] = [];implementors["tmp_mio"] = [];implementors["serde"] = [];implementors["hyper"] = [];implementors["serde"] = [];implementors["maidsafe_utilities"] = [];implementors["crust"] = [];
+implementors["antidote"] = [];implementors["lazy_static"] = [];implementors["either"] = [];implementors["humantime"] = [];implementors["libc"] = [];implementors["openssl"] = [];implementors["rust_sodium_sys"] = [];implementors["serde"] = [];implementors["tmp_mio"] = [];implementors["mio"] = [];implementors["hyper"] = [];implementors["serde"] = [];implementors["maidsafe_utilities"] = [];implementors["crust"] = [];
 
             if (window.register_implementors) {
                 window.register_implementors(implementors);
