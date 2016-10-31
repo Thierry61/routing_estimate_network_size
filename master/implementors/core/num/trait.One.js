4a7fd862d2eae1c65b9fe11421f9bad2110be790
MaidSafe-QA <qa@maidsafe.net>
2016-10-31 17:46:47 +0000

    Updated documentation.

--- master/implementors/core/num/trait.One.js
+++ master/implementors/core/num/trait.One.js
@@ -1,5 +1,5 @@
 (function() {var implementors = {};
-implementors["either"] = [];implementors["lazy_static"] = [];implementors["libc"] = [];implementors["hyper"] = [];implementors["serde"] = [];implementors["serde"] = [];
+implementors["either"] = [];implementors["lazy_static"] = [];implementors["libc"] = [];implementors["serde"] = [];implementors["hyper"] = [];implementors["serde"] = [];
 
             if (window.register_implementors) {
                 window.register_implementors(implementors);
--- master/implementors/core/str/pattern/trait.ReverseSearcher.js
+++ master/implementors/core/str/pattern/trait.ReverseSearcher.js
@@ -1,5 +1,5 @@
 (function() {var implementors = {};
-implementors["either"] = [];implementors["lazy_static"] = [];implementors["antidote"] = [];implementors["humantime"] = [];implementors["regex_syntax"] = [];implementors["libc"] = [];implementors["openssl"] = [];implementors["rust_sodium_sys"] = [];implementors["tmp_mio"] = [];implementors["mio"] = [];implementors["serde"] = [];implementors["serde"] = [];implementors["maidsafe_utilities"] = [];implementors["igd"] = [];implementors["crust"] = [];
+implementors["either"] = [];implementors["humantime"] = [];implementors["lazy_static"] = [];implementors["antidote"] = [];implementors["regex_syntax"] = [];implementors["libc"] = [];implementors["rust_sodium_sys"] = [];implementors["openssl"] = [];implementors["mio"] = [];implementors["tmp_mio"] = [];implementors["serde"] = [];implementors["serde"] = [];implementors["maidsafe_utilities"] = [];implementors["igd"] = [];implementors["crust"] = [];
 
             if (window.register_implementors) {
                 window.register_implementors(implementors);
