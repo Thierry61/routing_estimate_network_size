fc1482204980aa545f315e8c45905dc9ba7206b6
MaidSafe-QA <qa@maidsafe.net>
2016-11-01 15:09:23 +0000

    Updated documentation.

--- master/implementors/core/ops/trait.FnMut.js
+++ master/implementors/core/ops/trait.FnMut.js
@@ -1,5 +1,5 @@
 (function() {var implementors = {};
-implementors["either"] = [];implementors["humantime"] = [];implementors["lazy_static"] = [];implementors["antidote"] = [];implementors["libc"] = [];implementors["rust_sodium_sys"] = [];implementors["openssl"] = [];implementors["mio"] = [];implementors["tmp_mio"] = [];implementors["serde"] = [];implementors["hyper"] = [];implementors["serde"] = [];implementors["maidsafe_utilities"] = [];implementors["crust"] = [];
+implementors["lazy_static"] = [];implementors["either"] = [];implementors["antidote"] = [];implementors["humantime"] = [];implementors["libc"] = [];implementors["rust_sodium_sys"] = [];implementors["openssl"] = [];implementors["serde"] = [];implementors["mio"] = [];implementors["tmp_mio"] = [];implementors["serde"] = [];implementors["hyper"] = [];implementors["maidsafe_utilities"] = [];implementors["crust"] = [];
 
             if (window.register_implementors) {
                 window.register_implementors(implementors);
