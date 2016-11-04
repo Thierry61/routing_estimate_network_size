527b7dd02bacd0c7fa01451c1cf8895aac6c8abe
MaidSafe-QA <qa@maidsafe.net>
2016-11-04 11:49:40 +0000

    Updated documentation.

--- master/implementors/core/str/pattern/trait.DoubleEndedSearcher.js
+++ master/implementors/core/str/pattern/trait.DoubleEndedSearcher.js
@@ -1,5 +1,5 @@
 (function() {var implementors = {};
-implementors["humantime"] = [];implementors["antidote"] = [];implementors["either"] = [];implementors["lazy_static"] = [];implementors["regex_syntax"] = [];implementors["libc"] = [];implementors["openssl"] = [];implementors["rust_sodium_sys"] = [];implementors["tmp_mio"] = [];implementors["mio"] = [];implementors["serde"] = [];implementors["serde"] = [];implementors["igd"] = [];implementors["maidsafe_utilities"] = [];implementors["crust"] = [];
+implementors["lazy_static"] = [];implementors["antidote"] = [];implementors["either"] = [];implementors["serde"] = [];implementors["humantime"] = [];implementors["libc"] = [];implementors["regex_syntax"] = [];implementors["tmp_mio"] = [];implementors["rust_sodium_sys"] = [];implementors["openssl"] = [];implementors["mio"] = [];implementors["serde"] = [];implementors["igd"] = [];implementors["maidsafe_utilities"] = [];implementors["crust"] = [];
 
             if (window.register_implementors) {
                 window.register_implementors(implementors);
--- master/implementors/std/io/trait.Seek.js
+++ master/implementors/std/io/trait.Seek.js
@@ -1,5 +1,5 @@
 (function() {var implementors = {};
-implementors["humantime"] = [];implementors["antidote"] = [];implementors["openssl"] = [];implementors["rust_sodium_sys"] = [];implementors["tmp_mio"] = [];implementors["mio"] = [];implementors["maidsafe_utilities"] = [];implementors["crust"] = [];
+implementors["antidote"] = [];implementors["humantime"] = [];implementors["tmp_mio"] = [];implementors["rust_sodium_sys"] = [];implementors["openssl"] = [];implementors["mio"] = [];implementors["maidsafe_utilities"] = [];implementors["crust"] = [];
 
             if (window.register_implementors) {
                 window.register_implementors(implementors);
