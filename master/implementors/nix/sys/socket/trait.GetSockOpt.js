d8630c6d6a041e3f2b30db59e9ee8142ce8086f1
MaidSafe-QA <qa@maidsafe.net>
2016-11-18 19:01:00 +0000

    Updated documentation.

--- master/implementors/nix/sys/socket/trait.GetSockOpt.js
+++ master/implementors/nix/sys/socket/trait.GetSockOpt.js
@@ -1,5 +1,5 @@
 (function() {var implementors = {};
-implementors["nix"] = [];implementors["tmp_mio"] = [];implementors["mio"] = [];implementors["ws"] = [];implementors["maidsafe_utilities"] = [];implementors["crust"] = [];
+implementors["nix"] = [];implementors["mio"] = [];implementors["tmp_mio"] = [];implementors["ws"] = [];implementors["maidsafe_utilities"] = [];implementors["crust"] = [];
 
             if (window.register_implementors) {
                 window.register_implementors(implementors);
