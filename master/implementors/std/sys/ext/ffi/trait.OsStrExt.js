d1c9c814853b3264feadc6f6e4b74a952e6ea052
MaidSafe-QA <qa@maidsafe.net>
2016-11-23 16:02:18 +0000

    Updated documentation.

--- master/implementors/std/sys/ext/ffi/trait.OsStrExt.js
+++ master/implementors/std/sys/ext/ffi/trait.OsStrExt.js
@@ -1,5 +1,5 @@
 (function() {var implementors = {};
-implementors["lazy_static"] = [];implementors["antidote"] = [];implementors["humantime"] = [];implementors["libc"] = [];implementors["regex_syntax"] = [];implementors["num"] = [];implementors["itertools"] = [];implementors["serde"] = [];implementors["rust_sodium_sys"] = [];implementors["serde"] = [];implementors["openssl"] = [];implementors["chrono"] = [];implementors["nix"] = [];implementors["tmp_mio"] = [];implementors["mio"] = [];implementors["hyper"] = [];implementors["ws"] = [];implementors["maidsafe_utilities"] = [];implementors["igd"] = [];implementors["crust"] = [];
+implementors["lazy_static"] = [];implementors["antidote"] = [];implementors["humantime"] = [];implementors["regex_syntax"] = [];implementors["libc"] = [];implementors["num"] = [];implementors["itertools"] = [];implementors["serde"] = [];implementors["serde"] = [];implementors["rust_sodium_sys"] = [];implementors["openssl"] = [];implementors["chrono"] = [];implementors["nix"] = [];implementors["mio"] = [];implementors["tmp_mio"] = [];implementors["hyper"] = [];implementors["ws"] = [];implementors["maidsafe_utilities"] = [];implementors["igd"] = [];implementors["crust"] = [];
 
             if (window.register_implementors) {
                 window.register_implementors(implementors);
